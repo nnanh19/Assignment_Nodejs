c31bdd02ad25808310ac37c6f0e81617af1408ac
Nhật Anh <92773027+nnanh19@users.noreply.github.com>
2022-03-29 17:19:41 +0700

    update: ProductModel

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,7 +2,26 @@ import mongoose from "mongoose";
 const {Schema} = mongoose;
 
 const ProductSchema = new Schema({
-    "name" : String,
+    "name" : {
+        type : String,
+        required : true
+    },
+    "desc" : {
+        type : String,
+        required : true
+    },
+    "price" : {
+        type : Number,
+        required : true
+    },
+    "sale" : {
+        type : Number
+    },
+    "img" : {
+        type : String,
+        default : 'https://th.bing.com/th/id/R.06353653c4c75ea7099ac6a3dc86af33?rik=smriBRsIMGfd%2bQ&pid=ImgRaw&r=0'
+    },
+
     "slug" :{ 
             type: String ,
             lowercase : true,
--- src/routes/router.js
+++ src/routes/router.js
@@ -4,15 +4,17 @@ import UserController from "../controllers/UserController.js";
 import CheckAuth from "../middleware/checkAuth.js";
 const apiRoute = Router();
 
-
+//product
 apiRoute.get('/product/' , ProductController.index);
 
 apiRoute.post('/product/:userId' , CheckAuth.requiredSignin ,
                 CheckAuth.isAuth , CheckAuth.isAdmin, ProductController.create);
 
+//auth
 apiRoute.post('/signup', UserController.signup )
 apiRoute.post('/signin', UserController.signin )
 
+//param
 apiRoute.param("userId" , UserController.userById)
 
 export default apiRoute;
